281489b0f7f4cef75c2b05b9be6411fea138392a
Neto Jocelino <netojocelino@gmail.com>
2017-09-11 20:36:32 -0300

    Interface para Metodo de Brent

---
+++ src/Enem/index/Ordenador/IBrentOrganizer.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ordenador;
+
+import ENEM.Aluno;
+import java.nio.ByteBuffer;
+
+/**
+ *
+ * @author Jocelino Neto
+ */
+public interface IBrentOrganizer {
+    
+    /**
+     * TODO - Função HASH que calcula posição.
+     * @param matricula
+     * @return 
+     */
+    public long getHash(long matricula);
+    
+    /**
+     * TODO - Função que calcula quantidade de incrementos em posição.
+     * @param matricula
+     * @return 
+     */
+    public long getIncremeto(long matricula);
+    
+    /**
+     * TODO - Este método irá verificar a nova posição de um registro caso aja
+     * choque em uma posição.
+     * @param posicaoAtual
+     * @param qtdSaltos
+     * @param incremento
+     * @return 
+     */
+    public long contarSaltos(long posicaoAtual, long qtdSaltos, long incremento);
+    
+    /**
+     * TODO - Este metodo troca a posição de um registro e retorna um ByteBuffer
+     * caso exista um registro na posição de troca.
+     * @param aluno
+     * @param posicaoAtual
+     * @return 
+     */
+    public ByteBuffer trocarRegistro(Aluno aluno, long posicaoAtual);
+    
+    
+    public boolean armazenamentoVazio(ByteBuffer bb, long posicao);
+    
+}
